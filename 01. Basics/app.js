1f82d25be272e705388a41522b2ec1de7ee10c25
Manoj Chand <memanojkumain@gmail.com>
2025-09-27 16:08:00 +0530

    learned about variables, how to define a variable, how to declare a variable

--- 01. Basics/app.js
+++ 01. Basics/app.js
@@ -21,6 +21,27 @@ console.clear();
 // Shortcut to write console.log -> write clg and press tab
 
 
+// Variable is JS
+// (reserved keyword) (variable name) (type of value)
+
+//Declaring a variable
+
+
+// 1. Create variable name (name) and store your name
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (geneder) and store your gender
+// 4. Create variable name (twitterHandle) and store your twitter handle.
+// 5. Log all varialbes to the console.
+
+let fullName = "Manoj";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+
+console.log(fullName);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+
+
 
 
 

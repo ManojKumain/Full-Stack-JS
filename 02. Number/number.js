a3c1ea3824c8a7070329ea3ad39ca9db4d16ad4a
Manoj Chand <memanojkumain@gmail.com>
2025-09-27 17:10:05 +0530

    learned

---
+++ 02. Number/number.js
@@ -0,0 +1,128 @@
+let num = 2;
+
+// Basic Math
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// let counter = 10;
+// counter--;
+// counter--;
+// counter--;
+// console.log(counter);
+
+// 1. Create variable name (firstFavNum) & store your favorite number.
+// 2. Create variable name (secondFavNum) & store your favorite number.
+// 3. Add (firstFavNum) & store your favorite number.
+// 4. Subtract (firstFavNum & secondFavNum).
+// 5. Multiply (firstFavNum & secondFavNum).
+// 6. Divide (firstFavNum & secondFavNum).
+// 7. Check (firstFavNum Mod secondFavNum).
+// 8. Check the power of (firstFavNum).
+
+// let firstFavNum = 5;
+// let secondFavNum = 10;
+
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** 2);
+
+// BOOLEAN IN JS
+// let number = 10;
+// console.log(number + undefined);
+
+// FALSY VALUE IN JS
+// -> false
+// -> null
+// -> undefined
+// -> 0
+// -> -0
+// -> NaN
+// -> '', "", ``, (empty quotes)
+
+
+// 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
+// 2. Create Variable name (isJsHard) & store false as a value.
+// 3. Create Variable name (favNumb) & store your favourite number inside.
+// 4. Now add favNumber with the value of undefined
+
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 23;
+// console.log(isJsProgrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNumb + undefined);
+
+// COMPARISION OPERATORS
+// -> Relational Operators
+// >, <, >=, <=
+// console.log(10 > 10); 
+// console.log(10 < 10);
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+// ---------------------
+
+
+// -> Equality Operators
+// === strict equality (type + value)
+//  ==! strict non-equality operators (type + value)
+// == lose equality operator (value)
+// console.log(10===10);
+// console.log(10==='10');
+// console.log(10!=='10');
+// console.log(10 == 10);
+// console.log(10 == '10');
+
+
+// STRINGS IN JS
+let firstName = 'Manoj';
+let secondName = 'Chand';
+
+// 1. Concatenation
+// let fullName = firstName.concat(secondName);
+// console.log(fullName);
+
+// 2. Append
+// firstName += "something else"
+// console.log(firstName);
+
+// 3. Length
+// console.log(firstName.length);
+
+// 4. Uppercase
+// console.log(firstName.toUpperCase())
+
+// 5. Lowercase
+// console.log(firstName.toLowerCase());
+
+// 6. Slice
+// console.log(firstName.slice(0,1));
+// console.log(firstName.slice(0,2));
+// console.log(firstName.slice(0,3));
+// console.log(firstName.slice(0,4));
+// console.log(firstName.slice(2,5));
+
+// 7. Split & Join
+// console.log(firstName.split('').join(''));
+
+// 8. Includes
+// console.log(firstName.includes('M'))
+
+// 9. Trim
+// console.log(firstName.trim());
+
+// let fullName = `${firstName} ${secondName}`
+// console.log(fullName)
+
+
+
+
+
+
+

a9b1802348450dd43aca00b2095194b7e0be742d
Manoj Chand <memanojkumain@gmail.com>
2025-09-27 13:56:27 +0530

    learned about console.log, some methods related to console object such as console.error(), console.warn(), console.table(), console.clear(). at last learned about the shortcut to write console.log faster

---
+++ 01. Basics/app.js
@@ -0,0 +1,26 @@
+// First of all the console here is object
+console.log('Manoj This Side');
+
+console.log(2 + 2);
+
+console.log({name: 'alex'});
+
+// Some methods of console object
+// 1. console.error
+console.error('This is an error method');
+
+// 2. console.warn
+console.warn("This is a warning");
+
+// 3. console.table
+console.table({name: 'Manoj', age: 20});
+
+// 4. console.clear
+console.clear();
+
+// Shortcut to write console.log -> write clg and press tab
+
+
+
+
+

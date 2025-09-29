719d218cfe8ffacc1ba1f90eee7d23d7b645ce7f
Manoj Chand <memanojkumain@gmail.com>
2025-09-29 18:08:09 +0530

    Learned about conditional statements in js like if else and elseif

---
+++ 04. Control Flow/stript.js
@@ -0,0 +1,54 @@
+// CONDITIONAL STATEMENTS
+
+// if(condition) {...}
+// else if (condition) {...}
+// else {...}
+
+// First Example
+// let a = 20;
+// let b = 20;
+
+// if (a > b) {
+//     console.log("a is greater then b");
+// } else if(a < b) {
+//     console.log("a is less than b");
+// } else {
+//     console.log('a is equal to b')
+// }
+
+
+// Second Example
+// let time = 10;
+// let greetings;
+
+// if (time < 10) {
+//     greetings = "Good Morning";
+//     console.log(greetings);
+// } else if (time < 20) {
+//     greetings = "Good Day";
+//     console.log(greetings);
+// } else {
+//     greetings = "Good Evening";
+//     console.log(greetings);
+// }
+
+
+// 1. Create  variable name (password) store nothing
+// 2. If password is equal to 8 print "Welcome"
+// 3. If password is less then or equal to 8 print "Password is too short"
+// 4. If password is greater then or equalt to 8 print "Too Long Password" & "Password should be 8 characters"
+// 5. If all fails print "Please provide a password."
+
+// let password = 6;
+
+// if (password == 8) {
+//     console.log("Welcome");
+// } else if(password <= 8){
+//     console.log("Password is too short");
+// } else if(password >= 8){
+//     console.log("Too Long Password. Password should be 8 characters")
+// } else {
+//     console.log("Please provide a password")
+// }
+
+

02dcc69295f485998fab028744401c9f88bae794
Manoj Chand <memanojkumain@gmail.com>
2025-09-29 17:44:07 +0530

    In this I learned about type conversion

---
+++ 03. Type Conversion/typeConversion.js
@@ -0,0 +1,22 @@
+// Type Conversion
+// let money = '50';
+
+// Convert String to number
+// money = parseInt(money);
+// money = +money;
+// money = Number(money);
+
+// console.log(typeof money);
+
+
+// CONVERTING NUMBER TO STRING
+// let money = 50;
+// money = money.toString(); 
+// money = String(money);
+
+
+// CONVERTING STRING TO DECIMAL
+let money = '50.245';
+// money = parseFloat(money);
+
+console.log(typeof money);

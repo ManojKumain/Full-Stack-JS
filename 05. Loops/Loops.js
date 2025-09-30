cfeec56dc5d2111e3a0eb009727202f9ef3f3935
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 07:34:40 +0530

    Learned about do while loop + completed the challenge of do while loop

--- 05. Loops/Loops.js
+++ 05. Loops/Loops.js
@@ -33,3 +33,21 @@
 // }
 
 
+// DO WHILE LOOP IN JS
+
+// let i = 1;
+// do {
+//     console.log("Hello World", i);
+//     i++;
+// } while (i <= 5);
+
+
+// CHALLENGE FOR DO WHILE LOOP
+// Print "Your Name" 400 times to the console using do-while-loop. Iteration should start from 20.
+
+let i = 20;
+do {
+    console.log("Manoj", i);
+    i++;
+} while (i <= 400);
+

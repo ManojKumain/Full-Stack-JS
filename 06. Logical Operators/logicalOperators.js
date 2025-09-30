fdb25a115a646c0064b9511f8b6086fb86e68412
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 07:45:55 +0530

    Learned about logical operators in Js

---
+++ 06. Logical Operators/logicalOperators.js
@@ -0,0 +1,29 @@
+// LOGICAL OPERATORS IN JS
+
+// 1. Logical AND (&&)
+const a = true;
+const b = false;
+const c = 4;
+// console.log(a && b);
+// console.log(c < 2 && c > 2);
+
+
+// 2. Logical OR (||)
+// console.log(a || b);
+// console.log(b || b);
+// console.log(c > 2 || c < 2);
+
+// 3. Logical NOT (!)
+// console.log(!a);
+// console.log(!b);
+
+// let password = "manoj-webdev";
+// if (password.length >=8 && password.includes('web')){
+//     console.log("valid password");
+// } else {
+//     console.log("Invalid Password");
+// }
+
+let isTrue = true;
+console.log(!isTrue);
+

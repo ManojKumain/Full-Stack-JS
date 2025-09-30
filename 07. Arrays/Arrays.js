a93c5096624451b7cfe34aeddc8bd7330022e681
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 08:06:25 +0530

    Learned about array, nested arrays and how to access elements of an array

---
+++ 07. Arrays/Arrays.js
@@ -0,0 +1,41 @@
+// Arrays -> 0 index based
+
+// const numbersArray = [1, 2, 3, 4, 5];
+// console.log(numbersArray);
+
+// const stringArr = ['eat', 'sleep', 'code', 'repeat'];
+// console.log(stringArr);
+
+// Accessing Items from our array
+// console.log(stringArr[0]);
+// console.log(stringArr[1]);
+// console.log(stringArr[2]);
+// console.log(stringArr[3]);
+
+
+// Nested Array -> 2 dimension array
+// const nestArr = ['one', ['two', 'three'], 1, true, false];
+// console.log(nestArr);
+
+// Access elements of nested array
+// console.log(nestArr[1][1]);
+
+// const myLetters = ['h', 'e', 'l', 'l', 'o'];
+// console.log(myLetters);
+
+// 1. Create array name (favSingers) and store 3 favorite singers.
+// 2. log the first singer in that array
+// 3. Create array name (favNumbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) and store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+const favSingers = ['Atif', 'sonu', 'udit'];
+console.log(favSingers[0]);
+const favNumbers = [1, 4, 7, 9];
+const mixedArr = ['Manoj', ['one', 'two'], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[1][0]);
+console.log(mixedArr[1][1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);

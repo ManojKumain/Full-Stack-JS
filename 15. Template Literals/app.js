b28a529111cdefd165c455617de99f36164cc6e3
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 12:25:58 +0530

    In this tutorial I have learned about template literal in JS

---
+++ 15. Template Literals/app.js
@@ -0,0 +1,33 @@
+// Template Strings and Template Literals
+// console.log(`This is some random text`);
+// const firstName = 'Manoj';
+// const lastName = 'Kumain'
+
+// console.log(`My first name is: ${firstName} and my last name is ${lastName}`);
+
+// function info() {
+//     return `My name is manoj & I am 26 years old.`
+// }
+
+// console.log(`${info()}`);
+
+
+// Template literals exercise
+// 1. Store this in template string
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+
+// 2. Create two variable (firstName, laseName)
+// 3. Use that variable in template string.
+
+console.log(`The quick
+    brown fox
+    jumps over
+    the lazy dog
+    `);
+
+let firstName = "Manoj";
+let lastName = "Kumain";
+console.log(`${firstName} ${lastName}`);

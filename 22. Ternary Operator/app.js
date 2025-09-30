b57963154cf5e18232bc650d62a34bc056051100
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 17:12:57 +0530

    In this tutorial I have learned about the ternary operator in JS

---
+++ 22. Ternary Operator/app.js
@@ -0,0 +1,31 @@
+// The ternary operator in JavaScript is a concise way of writing conditional expressions. It is the only JavaScript operator that takes three operands: a condition followed by the question mark (?), an expression to execute if the condition is true, followed by a colon (:), and an expression to execute if the condition is false.
+// condition ? exprIfTrue : exprIffalse;
+
+let password = 2;
+
+// Without ternary operator
+// function passwordChecker(ps) {
+    // if (ps == 8){
+    //     return 'Strong Password';
+    // } else {
+    //     return 'Password should be 8 characters.';
+    // }
+    // return ps == 8 ? 'Strong Password' : "Password should be 8 characters."
+// }
+// const res = passwordChecker(password);
+// console.log(res);
+
+// const age = 25;
+// const isAdult = age >=18 ? 'Adult' : 'Not adult'
+// console.log(isAdult)
+
+
+// Challenge for Ternary Operator
+// If the person have money they can "buy products"
+// If a person don't have any money "They should bring money"
+
+// let money = 20;
+// const buyProduct = money > 0 ? "Buy products" : "They should bring money";
+// console.log(buyProduct)
+
+

ed72957cf0911f648fdf445a342275ae4522704e
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 11:13:07 +0530

    In this tutorial I learned about scope in JS

---
+++ 10. Scope/scope.js
@@ -0,0 +1,21 @@
+// Scopes in JS
+
+// 1. Global Variable
+// let msg = 'hi';
+// console.log(msg);
+
+// 2. Local Variable
+
+function showMessage() {
+    // let msg = "Hello";
+    // console.log(msg);
+    // console.log(msg);
+}
+
+// showMessage();
+
+
+// 3. Another Example
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}

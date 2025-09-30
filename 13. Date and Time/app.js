4231bac8c0b8e60bd7ef6d7f68fcdfcad2c80575
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 11:58:14 +0530

    In this tutorial I have learned about Date and Time in JS

---
+++ 13. Date and Time/app.js
@@ -0,0 +1,27 @@
+// const currentDate = new Date();
+// console.log(currentDate)
+
+// year, month, day, hours, minutes, seconds, miliseconds
+// const currentDate = new Date(2025, 9, 'tuesday', 11, 46, 0, 0);
+// console.log(currentDate);
+
+const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const miliSeconds = date.getMilliseconds();
+
+// console.log(`Year: ${year}`);
+// console.log(`Month: ${month}`);
+// console.log(`Day: ${day}`);
+// console.log(`Hours: ${hours}`);
+// console.log(`Minutes: ${minutes}`);
+// console.log(`MiliSeconds: ${miliSeconds}`);
+
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+// date.setDate(date.getDate() + 1);
+// console.log(date);

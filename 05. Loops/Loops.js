93a89870156f8790d7c670e51613ab7486149852
Manoj Chand <memanojkumain@gmail.com>
2025-09-30 07:25:06 +0530

    Learned about for loop + completed the challenge for for loop. Learned about while loop + completed it's challenge

---
+++ 05. Loops/Loops.js
@@ -0,0 +1,35 @@
+// For Loop
+// for (initialExpression; condition; incrementExpression) {}
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log("--- Outer Loop ---", i);
+//     // nested loop
+//     for (let j = 1; j <= 5; j++) {
+//         console.log("--- Inner Loop ---", j);
+//     }
+// }
+
+// Print your name & numbers from 0 through 1000
+// for(let i = 0; i <= 1000; i++) {
+//     console.log("Manoj", i);
+// }
+
+// WHILE LOOP
+// while (condition) { ... }
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log("Hello World", i);
+//   i++;  
+// }
+
+// WHILE LOOP CHALLENGE
+// Print "Your Name" 100 times to the console using while loop & iteration will start from 10
+
+// let i = 10;
+// while (i <= 100) {
+//     console.log("Manoj", i);
+//     i++;
+// }
+
+
